9dba41ccc275f297660fd21f4c63ff16315ee207
sreedharrapuru <sreedhar.rapuru1@t-mobile.com>
2019-03-20 23:09:06 -0700

    Added exceptions on Branch7

--- ppmtoolFullStack/src/main/java/io/agileinteligence/ppmtool/services/ProjectService.java
+++ ppmtoolFullStack/src/main/java/io/agileinteligence/ppmtool/services/ProjectService.java
@@ -3,6 +3,7 @@ package io.agileinteligence.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import io.agileinteligence.ppmtool.domain.project;
+import io.agileinteligence.ppmtool.exceptions.ProjectIdException;
 import io.agileinteligence.ppmtool.repositories.ProjectRepository;
 
 @Service 
@@ -13,6 +14,12 @@ public class ProjectService {
 
 	public project saveOrUpdateProject(project project) {
 		
-		return ProjectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return ProjectRepository.save(project);
+		}catch (Exception e) {
+			throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+		}
+		
 	}
 }
