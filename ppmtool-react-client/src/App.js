20af266463e4cb3e653d56280ac6e44da22c3f8b
sreedharrapuru <sreedhar.rapuru1@t-mobile.com>
2019-03-22 10:32:41 -0700

    fixed issue with Route

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
       <Router>
         <div className="App">
           <Header />
-          <Dashboard />
-          <Route path="/addProject" Component={AddProject} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
         </div>
       </Router>
     );
