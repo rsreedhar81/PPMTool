b8becc74f3081cb34a66fed1c9c52a8e6b961850
sreedharrapuru <sreedhar.rapuru1@t-mobile.com>
2019-03-24 01:43:53 -0700

    Projecttask relationship

--- ppmtoolFullStack/src/main/java/io/agileinteligence/ppmtool/domain/Backlog.java
+++ ppmtoolFullStack/src/main/java/io/agileinteligence/ppmtool/domain/Backlog.java
@@ -4,6 +4,8 @@ package io.agileinteligence.ppmtool.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Backlog {
@@ -21,6 +23,8 @@ public class Backlog {
     private project project;
 
     //OneToMany projecttasks
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "backlog")
+    private List<ProjectTask> projectTasks = new ArrayList<>();
 
 
     public Backlog() {
@@ -56,5 +60,13 @@ public class Backlog {
 
     public void setProject(project project) {
         this.project = project;
+    }
+
+    public List<ProjectTask> getProjectTasks() {
+        return projectTasks;
+    }
+
+    public void setProjectTasks(List<ProjectTask> projectTasks) {
+        this.projectTasks = projectTasks;
     }
 }
