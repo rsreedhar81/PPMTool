7d03c55fdaf3d7e531473b5bfa67b6ed4c694ced
sreedharrapuru <sreedhar.rapuru1@t-mobile.com>
2019-03-24 01:59:13 -0700

    persist project task

---
+++ ppmtoolFullStack/src/main/java/io/agileinteligence/ppmtool/web/BacklogController.java
@@ -0,0 +1,46 @@
+package io.agileinteligence.ppmtool.web;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.agileinteligence.ppmtool.domain.ProjectTask;
+import io.agileinteligence.ppmtool.services.MapValidationErrorService;
+import io.agileinteligence.ppmtool.services.ProjectTaskService;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask,
+                                            BindingResult result, @PathVariable String backlog_id){
+
+        ResponseEntity<?> erroMap = mapValidationErrorService.MapValidationErrorService(result);
+        if (erroMap != null) return erroMap;
+
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+
+    }
+
+
+}
