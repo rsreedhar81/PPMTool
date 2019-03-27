b38fc33a36759693ff602b6bc9c1d5f59bfeae50
sreedharrapuru <sreedhar.rapuru1@t-mobile.com>
2019-03-26 22:29:46 -0700

    Create userObject, validation, Repository, Service

---
+++ ppmtoolFullStack/src/main/java/io/agileinteligence/ppmtool/services/UserService.java
@@ -0,0 +1,16 @@
+package io.agileinteligence.ppmtool.services;
+
+
+import io.agileinteligence.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+}
